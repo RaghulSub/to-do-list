4d15138ec20debcce71695b733704cbb5ed9e4b9
RaghulSub <raghulsubramanain10@gmail.com>
2024-10-10 14:18:16 +0530

    made model attribute public so it can be accessed

--- BackEnd/models/models.ts
+++ BackEnd/models/models.ts
@@ -1,25 +1,32 @@
 import { Model, DataTypes } from "sequelize";
 import sequelize from "../database/config";
 
-class ToDoModel extends Model{}
+class ToDoModel extends Model {
+  public id!: number;
+  public title!: string;
+  public description!: string;
+  public completed!: boolean;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+}
 
 ToDoModel.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: DataTypes.STRING,
-        description: DataTypes.TEXT,
-        completed: DataTypes.BOOLEAN,
-        createdAt: DataTypes.DATE,
-        updatedAt: DataTypes.DATE
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        modelName:'Todo'
-    }
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    completed: DataTypes.BOOLEAN,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize,
+    modelName: "Todo",
+  }
 );
 
 export default ToDoModel;
