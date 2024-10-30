ecf53819980211d033c485e140fe10bbef0d4413
RaghulSub <raghulsubramanain10@gmail.com>
2024-10-30 09:03:59 +0530

    updated controller with changed schema

--- backend/controller/todoController.ts
+++ backend/controller/todoController.ts
@@ -1,13 +1,12 @@
-import express from "express";
 import { Request, Response } from "express";
-import ToDoModel from "../models/models";
+import ToDoModel  from "../models/ToDo";
 
 export const InsertToDo = async (req: Request, res: Response) => {
   try {
     let data = req.body;
     let date = new Date();
     await ToDoModel.create({
-      id:data.id,
+      id: data.id,
       title: data.title,
       description: data.description,
       completed: false,
@@ -40,79 +39,78 @@ export const GetToDo = async (req: Request, res: Response) => {
   res.json(todos);
 };
 
-
-export const UpdateToDo = async(req:Request,res:Response) => {
-    try{
-        const { id,date } = req.body;
-        const selectedRows = await ToDoModel.findOne({
-            where:{
-                id:id,
-            },
-            attributes:{
-                include:['completed']
-            }
-        });
-        if(!selectedRows){
-            return res.status(404).json({
-                success:false,
-                message:"To-Do not found",
-            });
-        }
-        
-        const updatedRow = await  ToDoModel.update({
-           completed:!selectedRows.completed,
-           updatedAt: date,
-        },{
-            where:{
-                id:id,
-            }
-        });
-        if(updatedRow[0] === 0){
-            return res.status(404).json({
-                success:false,
-                message:"To-Do not found",
-            });
-        }
-        return res.status(200).json({
-            success:true,
-            message:"To-Do Updated Successfully",
-            data:selectedRows,
-        })
-
-    }catch(error){
-        return res.status(500).json({
-            success:false,
-            message:"Internal Server Error on UpdateToDo",
-            error:error,
-        });
-    }
-}
-
-
-export const DeleteToDo = async (req:Request,res:Response) => {
-  try{
-    const id = req.body.id;
-    const deletedRow = await ToDoModel.destroy({
-      where:{id:id}
+export const UpdateToDo = async (req: Request, res: Response) => {
+  try {
+    const { id, date } = req.body;
+    const selectedRows = await ToDoModel.findOne({
+      where: {
+        id: id,
+      },
+      attributes: {
+        include: ["completed"],
+      },
     });
-    if(deletedRow == 0){
+    if (!selectedRows) {
       return res.status(404).json({
-        success:false,
-        message:"Record Not Found",
-      })
+        success: false,
+        message: "To-Do not found",
+      });
+    }
+
+    const updatedRow = await ToDoModel.update(
+      {
+        completed: !selectedRows.completed,
+        updatedAt: date,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    if (updatedRow[0] === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "To-Do not found",
+      });
     }
     return res.status(200).json({
-      success:true,
-      message:'To-Do Deleted Successfully',
-      data:`No of Records Deleted ${deletedRow}`,
-    })
-    
-  }catch(error){
-    console.log(error);
+      success: true,
+      message: "To-Do Updated Successfully",
+      data: selectedRows,
+    });
+  } catch (error) {
     return res.status(500).json({
-      success:false,
-      message:"Internal Server Error",
-      error:error,
+      success: false,
+      message: "Internal Server Error on UpdateToDo",
+      error: error,
     });
   }
-}
+};
+
+export const DeleteToDo = async (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
+    const deletedRow = await ToDoModel.destroy({
+      where: { id: id },
+    });
+    if (deletedRow == 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Record Not Found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "To-Do Deleted Successfully",
+      data: `No of Records Deleted ${deletedRow}`,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
+};
